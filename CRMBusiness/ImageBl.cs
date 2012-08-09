d51254e05b0d638e7a8d07216efc50b459fdb185
josh1zn <josh1zn@62e02c76-00fc-81ac-00c0-e4422755b3ec>
2012-08-09 13:44:45 +0000

    ImageBl.cs NoteBl.cs and SolutionBl.cs are fine but ProblemBl.cs GetProblem(description) method needs to be discussed.
    the method searches for problems by -- LIKE '%description%' -- 
    is it okay or not?

---
+++ CRMBusiness/ImageBl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CRMBusiness.CRM;
+
+namespace CRMBusiness
+{
+    public class ImageBl
+    {
+        private CRMEntities _crm;
+        private readonly Uri _uri = new Uri("http://localhost:1677/CRMService.svc");
+
+        //Adds a new image to database
+        public void AddImage(byte[] imagefile, int epid)
+        {
+            _crm = new CRMEntities(_uri);
+            var i = new Image {ImageFile = imagefile, EP_ID = epid};
+            _crm.AddToImages(i);
+            _crm.SaveChanges();
+        }
+
+        //Gets all images for a specific problem(EP_ID)
+        public List<Image> GetImages(int epid)
+        {
+            _crm = new CRMEntities(_uri);
+            return _crm.Images.Where(i => i.EP_ID == epid).ToList();
+        }
+    }
+}
