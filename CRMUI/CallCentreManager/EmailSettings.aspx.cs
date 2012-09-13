6c3affacb831162e0367ada98f17ebbba0a2f025
nnn.williamson@gmail.com <nnn.williamson@gmail.com@62e02c76-00fc-81ac-00c0-e4422755b3ec>
2012-09-13 21:15:53 +0000

    Input validation and value population on load added

--- CRMUI/CallCentreManager/EmailSettings.aspx.cs
+++ CRMUI/CallCentreManager/EmailSettings.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Globalization;
 using System.Net.Configuration;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using Ext.Net;
 using System.Web.Configuration;
 using System.Configuration;
@@ -16,14 +12,43 @@ namespace CRMUI.CallCentreManager
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
+                var mailsection = config.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+                
 
+
+                if(!IsPostBack)
+                {
+                    if (mailsection != null)
+                    {
+                        var loadsmtp = mailsection.Smtp;
+                        txtServer.Text = loadsmtp.Network.Host.ToString(CultureInfo.InvariantCulture);
+                        txtUsername.Text = loadsmtp.Network.UserName;
+                        txtPort.Text = loadsmtp.Network.Port.ToString(CultureInfo.InvariantCulture);
+                        chkEnableSSL.Checked = loadsmtp.Network.EnableSsl;
+
+                    }
+                }
+            }
+            catch (Exception loadex)
+            {
+
+                ExtNet.MessageBox.Alert("Error", loadex.Message).Show();
+            }
+            
         }
 
         protected void SaveSettings(object sender, DirectEventArgs e)
         {
             try
             {
-                ValidateInputs();
+                //declarations
+                            //mailsettings
+                Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
+                var mailsection = config.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
+                            //check ssl
                 bool ssltype = false;
 
                 switch (chkEnableSSL.Checked)
@@ -35,10 +60,38 @@ namespace CRMUI.CallCentreManager
                         ssltype = false;
                         break;
                 }
-               
-               Configuration config = WebConfigurationManager.OpenWebConfiguration("~/");
-                var mailsection = config.GetSectionGroup("system.net/mailSettings") as MailSettingsSectionGroup;
-                if (mailsection != null)
+                //validate text input controls
+                //-----------------------
+                if (txtServer.Text == null)
+                {
+                    txtServer.Focus();
+                }
+                else if (txtUsername.Text == "")
+                {
+                    txtUsername.Focus();
+                }
+                else if (txtPassword.Text == "")
+                {
+                    txtPassword.Focus();
+                }
+                else if (txtPort.Text == "")
+                {
+                    txtPort.Focus();
+                }
+
+                else if (txtPassword.Text == txtConfirmPassword.Text)
+                {
+                    txtPassword.IndicatorIcon = Icon.Tick;
+                    txtConfirmPassword.IndicatorIcon = Icon.Tick;
+                }
+                else if (txtConfirmPassword!=txtPassword)
+                {
+                    txtPassword.IndicatorIcon = Icon.Exclamation;
+                    txtConfirmPassword.IndicatorIcon = Icon.Exclamation;
+                    txtConfirmPassword.IndicatorText = "Password do not match";
+                    txtPassword.Focus();
+                }
+                else if(mailsection != null)
                 {
                     var smtpSection = mailsection.Smtp;
 
@@ -51,7 +104,6 @@ namespace CRMUI.CallCentreManager
 
                     ExtNet.MessageBox.Notify("Save Status", "Email Settings updated successfuly!").Show();
                     Disablecontrols();
-                    
                 }
                 else
                 {
@@ -62,40 +114,7 @@ namespace CRMUI.CallCentreManager
             {
 
                 ExtNet.MessageBox.Alert("Error", ex.Message).Show();
-                ;
-            }
-        }
-
-        protected void ValidateInputs()
-        {
-            if(txtServer.Text == null)
-            {
-                txtServer.Focus();
-            }
-            else if (txtUsername.Text == "")
-            {
-                txtUsername.Focus();
-            }
-            else if (txtPassword.Text == "")
-            {
-                txtPassword.Focus();
-            }
-            else if (txtPort.Text == "")
-            {
-                txtPort.Focus();
-            }
-            
-            if (txtPassword.Text == txtConfirmPassword.Text)
-            {
-                txtPassword.IndicatorIcon = Icon.Tick;
-                txtConfirmPassword.IndicatorIcon = Icon.Tick;
-            }
-            else
-            {
-                txtPassword.IndicatorIcon = Icon.Exclamation;
-                txtConfirmPassword.IndicatorIcon = Icon.Exclamation;
-                txtConfirmPassword.IndicatorText = "Password do not match";
-                txtPassword.Focus();
+                
             }
         }
 
