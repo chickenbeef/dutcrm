557803fc3700f1c50a5b45999598cfee1ae66e92
nnn.williamson@gmail.com <nnn.williamson@gmail.com@62e02c76-00fc-81ac-00c0-e4422755b3ec>
2012-09-27 21:13:32 +0000

    EmailSettings page completed

--- trunk2/CRMUI/CallCentreManager/EmailSettings.aspx
+++ trunk2/CRMUI/CallCentreManager/EmailSettings.aspx
@@ -5,59 +5,65 @@
     <ext:ResourceManager ID="ResourceManager1" runat="server" Theme="Default" />
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="cphCallCMBody" runat="server">
-    <ext:Panel ID="pnlMain" runat="server" MinHeight="618" Title="Email Settings" LayoutConfig="HBoxLayout" MaxWidth="1349">
+    <ext:Panel ID="pnlMain" runat="server" MinHeight="618" Title="Email Settings" LayoutConfig="HBoxLayout"
+        MaxWidth="1349">
         <LayoutConfig>
-            <ext:HBoxLayoutConfig Align="Middle" Pack="Center"/>
+            <ext:HBoxLayoutConfig Align="Middle" Pack="Center" />
         </LayoutConfig>
         <Items>
-            <ext:FormPanel runat="server" ID="pnlEmailSettings" Width="500" Height="420" BodyPadding="30" Frame="True">
+            <ext:FormPanel runat="server" ID="pnlEmailSettings" Width="500" Height="420" BodyPadding="30"
+                Frame="True">
                 <Items>
                     <%--USER CREDENTIALS--%>
-                        <ext:TextField runat="server" ID="txtUsername" FieldLabel="Username" Anchor="80%" AllowBlank="False"
-                        IndicatorTip="It is required field" MsgTarget="Side" Vtype="email"/>
-
-                        <ext:TextField runat="server" ID="txtPassword" FieldLabel="Password" InputType="Password"
-                        Anchor="80%" AllowBlank="False" IndicatorTip="It is required field" MsgTarget="Side"/>
-
-                        <ext:TextField ID="txtConfirmPassword" runat="server" FieldLabel="Confirm Password" Vtype="password"
-                        InputType="Password" MsgTarget="Side" Anchor="80%" AllowBlank="False" IndicatorTip="It is required field"/>
-                        
-                        <%--INCOMING MAIL SETTINGS--%>
-                        <ext:FieldSet runat="server" ID="fdstIncomingConfigs" Title="Incoming Mail Settings">
-                            <Items>
-                                    <ext:TextField runat="server" ID="txtInServer" FieldLabel="(POP/IMAP)Mail Server" Anchor="80%" AllowBlank="False" 
-                                        IndicatorTip="It is required field" MsgTarget="Side"/>
-                        
-                                    <ext:TextField runat="server" ID="txtInPort" FieldLabel="Incoming Port"  Anchor="50%" AllowBlank="False" IndicatorTip="It is required field"/>
-
-                                    <ext:Checkbox runat="server" ID="chkEnableInSSL" FieldLabel="Use SSL" />
-                            </Items>
-                        </ext:FieldSet>
-                        
-                        <%--OUTGOING MAIL SETTINGS--%>
-                        
-                        <ext:FieldSet runat="server" ID="fdstOutgoingConfigs" Title="Outgoing Mail Settings">
-                            <Items>
-                                    <ext:TextField runat="server" ID="txtOtServer" FieldLabel="(SMTP) Mail Server" Anchor="80%" AllowBlank="False" 
-                                        IndicatorTip="It is required field" MsgTarget="Side"/>
-                        
-                                    <ext:TextField runat="server" ID="txtOtPort" FieldLabel="Outgoing Port"  Anchor="50%" AllowBlank="False" IndicatorTip="It is required field"/>
-
-                                    <ext:Checkbox runat="server" ID="chkEnableOtSSL" FieldLabel="Use SSL" />
-                            </Items>
-                        </ext:FieldSet>
-                        
-                        <ext:Container runat="server">
-                            <LayoutConfig>
-                                <ext:HBoxLayoutConfig Align="Middle" Pack="Center"/>
-                            </LayoutConfig>
-                                <Items>
-                                        <ext:Button runat="server" ID="BtnSave" Text="Save" Icon="Disk" Padding="5" OnDirectClick="SaveSettings" />
-                                </Items>
-                        </ext:Container>
-                
+                    <ext:TextField runat="server" ID="txtUsername" FieldLabel="Username" Anchor="80%"
+                        IndicatorTip="It is required field" MsgTarget="Side" IsRemoteValidation="True">
+                        <RemoteValidation OnValidation="ValidateUsername" />
+                    </ext:TextField>
+                    <ext:TextField runat="server" ID="txtPassword" FieldLabel="Password" InputType="Password"
+                        Anchor="80%" AllowBlank="False" IndicatorTip="It is required field" MsgTarget="Side"
+                        MinLength="6" MinLengthText="Minimum of 6 charachers required!" MaxLength="12"
+                        MaxLengthText="Maximum of 12 characters accepted!" />
+                    <ext:TextField ID="txtConfirmPassword" runat="server" FieldLabel="Confirm Password"
+                        Vtype="password" InputType="Password" MsgTarget="Side" Anchor="80%" IsRemoteValidation="True">
+                        <RemoteValidation OnValidation="ValidateConfirmPassord" />
+                    </ext:TextField>
+                    <%--INCOMING MAIL SETTINGS--%>
+                    <ext:FieldSet runat="server" ID="fdstIncomingConfigs" Title="Incoming Mail Settings">
+                        <Items>
+                            <ext:TextField runat="server" ID="txtInServer" FieldLabel="(POP/IMAP)Mail Server"
+                                Anchor="80%" MsgTarget="Side" IsRemoteValidation="True">
+                                <RemoteValidation OnValidation="ValidateIncomingServer" />
+                            </ext:TextField>
+                            <ext:TextField runat="server" ID="txtInPort" FieldLabel="Incoming Port" Anchor="50%"
+                                MsgTarget="Side" IsRemoteValidation="True">
+                                <RemoteValidation OnValidation="ValidateIncomingPort" />
+                            </ext:TextField>
+                            <ext:Checkbox runat="server" ID="chkEnableInSSL" FieldLabel="Use SSL" />
+                        </Items>
+                    </ext:FieldSet>
+                    <%--OUTGOING MAIL SETTINGS--%>
+                    <ext:FieldSet runat="server" ID="fdstOutgoingConfigs" Title="Outgoing Mail Settings">
+                        <Items>
+                            <ext:TextField runat="server" ID="txtOtServer" FieldLabel="(SMTP) Mail Server" Anchor="80%"
+                                MsgTarget="Side" IsRemoteValidation="True">
+                                <RemoteValidation OnValidation="ValidateOutgoingServer" />
+                            </ext:TextField>
+                            <ext:TextField runat="server" ID="txtOtPort" FieldLabel="Outgoing Port" Anchor="50%"
+                                IsRemoteValidation="True" MsgTarget="Side">
+                                <RemoteValidation OnValidation="ValidateOutgoingPort" />
+                            </ext:TextField>
+                            <ext:Checkbox runat="server" ID="chkEnableOtSSL" FieldLabel="Use SSL" />
+                        </Items>
+                    </ext:FieldSet>
+                    <ext:Container runat="server">
+                        <LayoutConfig>
+                            <ext:HBoxLayoutConfig Align="Middle" Pack="Center" />
+                        </LayoutConfig>
+                        <Items>
+                            <ext:Button runat="server" ID="BtnSave" Text="Save" Icon="Disk" Padding="5" OnDirectClick="SaveSettings" />
+                        </Items>
+                    </ext:Container>
                 </Items>
-                
             </ext:FormPanel>
         </Items>
     </ext:Panel>
--- trunk2/CRMUI/CallCentreManager/EmailSettings.aspx.cs
+++ trunk2/CRMUI/CallCentreManager/EmailSettings.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.Text.RegularExpressions;
 using Ext.Net;
 using CRMBusiness;
 
@@ -8,6 +9,14 @@ namespace CRMUI.CallCentreManager
 {
     public partial class EmailSettings : System.Web.UI.Page
     {
+        #region PUBLIC VARIABLES FOR VALIDATION
+        public static bool Validun = true;
+        public static bool Validconfmpw = true;
+        public static bool Validincserver = true;
+        public static bool Validincport = true;
+        public static bool Validoutserver = true;
+        public static bool Validoutport = true;
+        #endregion
         protected void Page_Load(object sender, EventArgs e)
         {
             try
@@ -30,38 +39,40 @@ namespace CRMUI.CallCentreManager
             try
             {
 
-                //validate text input controls
-                //----------------------------
-                if (txtInServer.Text == null)
-                {
-                    txtInServer.Focus();
-                }
-                else if (txtUsername.Text == "")
+                //check if validation is met
+                if (Validun == false)
                 {
                     txtUsername.Focus();
+                    InvalidMessage();
                 }
-                else if (txtPassword.Text == "")
+                else if (Validconfmpw == false)
                 {
-                    txtPassword.Focus();
+                    txtConfirmPassword.Focus();
+                    InvalidMessage();
                 }
-                else if (txtInPort.Text == "")
+                else if (Validincserver == false)
+                {
+                    txtInServer.Focus();
+                    InvalidMessage();
+                }
+                else if (Validincport == false)
                 {
                     txtInPort.Focus();
+                    InvalidMessage();
                 }
-                else if (txtPassword.Text == txtConfirmPassword.Text)
+                else if (Validoutserver == false)
                 {
-                    txtPassword.IndicatorIcon = Icon.Tick;
-                    txtConfirmPassword.IndicatorIcon = Icon.Tick;
+                    txtOtServer.Focus();
+                    InvalidMessage();
                 }
-                else if (txtConfirmPassword != txtPassword)
+                else if (Validoutport == false)
                 {
-                    txtPassword.IndicatorIcon = Icon.Exclamation;
-                    txtConfirmPassword.IndicatorIcon = Icon.Exclamation;
-                    txtConfirmPassword.IndicatorText = "Password do not match";
-                    txtPassword.Focus();
+                    txtOtPort.Focus();
+                    InvalidMessage();
                 }
                 else
                 {
+                    //if all validation is met then save
                     if (Savealldata())
                     {
                         ExtNet.MessageBox.Notify("Save Status", "Email Settings updated successfuly!").Show();
@@ -93,6 +104,7 @@ namespace CRMUI.CallCentreManager
             txtOtServer.ReadOnly = true;
             txtOtPort.ReadOnly = true;
             chkEnableOtSSL.ReadOnly = true;
+            BtnSave.Disabled = true;
         }
 
         protected void Loadcurrentconfig()
@@ -160,6 +172,209 @@ namespace CRMUI.CallCentreManager
 
             saveconfigs.UpdateSetting("EmailAddr", txtUsername.Text);
             return true;
+        }
+        #endregion
+
+        #region VALIDATION
+        //validation for all fields
+        protected void InvalidMessage()
+        {
+            ExtNet.Msg.Alert("Error", "See fields marked with exclamations! /n Or reconfigure values before attempting to save!").Show();
+        }
+
+        protected void ValidateUsername(object sender, RemoteValidationEventArgs e)
+        {
+            Validun = false;
+            var un = (TextField)sender;
+
+            const string strRegex = @"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            var re = new Regex(strRegex);
+
+            if (un.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else
+            {
+                if (re.IsMatch(un.Text))
+                {
+                    e.Success = true;
+                    Validun = true;
+                }
+                else
+                {
+                    e.Success = false;
+                    e.ErrorMessage = "Not a valid mail address";
+                }
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        protected void ValidateConfirmPassord(object sender, RemoteValidationEventArgs e)
+        {
+            Validconfmpw = false;
+            var confirmpw = (TextField)sender;
+
+            if (confirmpw.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else if (confirmpw.Text.Length < 6)
+            {
+                e.Success = false;
+                e.ErrorMessage = "Minimum characters is 6!";
+            }
+            else if (confirmpw.Text.Length > 12)
+            {
+                e.Success = false;
+                e.ErrorMessage = "Maximum of 12 characters allowed!";
+            }
+            else
+            {
+                if (confirmpw.Text == txtPassword.Text)
+                {
+                    e.Success = true;
+                    Validconfmpw = true;
+                }
+                else
+                {
+                    e.Success = false;
+                    e.ErrorMessage = "Passwords do not match!";
+                }
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        protected void ValidateIncomingServer(object sender, RemoteValidationEventArgs e)
+        {
+            Validincserver = false;
+            var incserver = (TextField)sender;
+
+            const string strRegex = @"^([a-zA-Z0-9_\-\.]+)((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            var re = new Regex(strRegex);
+
+            if (incserver.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else
+            {
+                if (re.IsMatch(incserver.Text))
+                {
+                    e.Success = true;
+                    Validincserver = true;
+                }
+                else
+                {
+                    e.Success = false;
+                    e.ErrorMessage = "Not a valid server address";
+                }
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        protected void ValidateIncomingPort(object sender, RemoteValidationEventArgs e)
+        {
+            Validincport = false;
+            var incport = (TextField)sender;
+
+            if (incport.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else if (Checknum(incport.Text) == false)
+            {
+                e.Success = false;
+                e.ErrorMessage = "Port must be a number and between 1 and 999!";
+            }
+            else
+            {
+                e.Success = true;
+                Validincport = true;
+            }
+
+
+        }
+
+        protected void ValidateOutgoingServer(object sender, RemoteValidationEventArgs e)
+        {
+            Validoutserver = false;
+            var outserver = (TextField)sender;
+
+            const string strRegex = @"^([a-zA-Z0-9_\-\.]+)((\[[0-9]{1,3}" + @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" + @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$";
+            var re = new Regex(strRegex);
+
+            if (outserver.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else
+            {
+                if (re.IsMatch(outserver.Text))
+                {
+                    e.Success = true;
+                    Validoutserver = true;
+                }
+                else
+                {
+                    e.Success = false;
+                    e.ErrorMessage = "Not a valid server address";
+                }
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        protected void ValidateOutgoingPort(object sender, RemoteValidationEventArgs e)
+        {
+            Validoutport = false;
+            var outport = (TextField)sender;
+
+            if (outport.Text == "")
+            {
+                e.Success = false;
+                e.ErrorMessage = "Field is required!";
+            }
+            else if (Checknum(outport.Text) == false)
+            {
+                e.Success = false;
+                e.ErrorMessage = "Port must be a number and between 1 and 999!";
+            }
+            else
+            {
+                e.Success = true;
+                Validoutport = true;
+            }
+        }
+
+        protected bool Checknum(string port)
+        {
+            double retNum;
+            var isNum = Double.TryParse(Convert.ToString(port), System.Globalization.NumberStyles.Any,
+                                         System.Globalization.NumberFormatInfo.InvariantInfo, out retNum);
+            if (!isNum)
+            {
+                return false;
+            }
+            else
+            {
+                if (Convert.ToInt32(port) < 0)
+                {
+                    return false;
+                }
+                else if (Convert.ToInt32(port) > 999)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
         }
         #endregion
     }
